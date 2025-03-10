f761020aaeedb9779f761c70e4781ec790a3dd4f
ervinosmani3@gmail.com <ervinosmani3@gmail.com>
2025-03-10 11:51:28 +0100

    initial project setup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// Variabël për të kontrolluar menynë në ekranet e vogla
+const isOpen = ref(false);
+
+</script>
+
+<template>
+    <nav class="bg-gray-900">
+
+    </nav>
+</template>
