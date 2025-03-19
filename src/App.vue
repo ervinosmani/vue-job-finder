5c99c7a15a61c7ecb4cb246dacbac451af629093
ervinosmani3@gmail.com <ervinosmani3@gmail.com>
2025-03-19 11:28:04 +0100

    dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.documentElement.classList.add('dark'); 
+});
 </script>
 
 <template>
